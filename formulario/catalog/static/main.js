6dbe5ee11553e6b6ba708df2b81096f820ae359c
Alejandro <brayanalejandroruiz@gmail.com>
2024-02-24 20:08:33 -0500

    actualizo catalogo y saludo

--- formulario/catalog/static/main.js
+++ formulario/catalog/static/main.js
@@ -4,36 +4,58 @@ const apellidos = document.getElementById("apellidos");
 const correo = document.getElementById("email");
 const tel = document.getElementById("tel");
 const skill = document.getElementById("skill");
-const valido = function (elemento) {
-  if (elemento.value == '') {
-    console.log("Debe llenar el campo elemento.name")
-  }
-}
-function evento_blur_vacio(elemento) {
+const evento_blur_vacio = function (elemento) {
   elemento.addEventListener('blur', () => {
     if (elemento.value == "") {
       console.log(`Debe llenar el campo'${elemento.name}'.`);
-      elemento.style.borderColor = "green";
+      elemento.style.borderColor = "salmon";
     }
   })
 }
-const evento_blur_validar = function (elemento) {
+const evento_blur_ficha = function (elemento) {
   elemento.addEventListener('blur', () => {
     const patt_str = /^[a-zA-Z]{3}_[0-9]{5}$/;
     if (!patt_str.test(ficha.value)) {
       console.log(`Campo '${elemento.name}' invalido.`);
-      ficha.style.borderColor = 'red';
+      elemento.style.borderColor = 'red';
     } else {
-      ficha.style.borderColor = 'skyblue';
+      elemento.style.borderColor = 'skyblue';
     }
   })
 }
-evento_blur_validar(ficha);
+const evento_blur_email = function (elemento) {
+  elemento.addEventListener('blur', () => {
+    const patt_email = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!patt_email.test(elemento.value)) {
+      console.log(`Campo '${elemento.name}' invalido.`);
+      elemento.style.borderColor = "red";
+    } else {
+      console.log(`Campo '${elemento.name}' valido.`);
+      elemento.style.borderColor = 'skyblue';
+    }
+  })
+}
+const evento_blur_str = function (elemento) {
+  elemento.addEventListener('blur', () => {
+    const patt_str = /^[a-zA-Z]+/;
+    if (!patt_str.test(elemento.value)) {
+      console.log(`Campo '${elemento.name}' invalido.`);
+      elemento.style.borderColor = "red";
+    } else {
+      console.log(`Campo '${elemento.name}' válido.`);
+      elemento.style.borderColor = "skyblue";
+    }
+  })
+}
+evento_blur_ficha(ficha);
 evento_blur_vacio(ficha);
 evento_blur_vacio(nombre);
+evento_blur_str(nombre);
 evento_blur_vacio(apellidos);
+evento_blur_str(apellidos);
 evento_blur_vacio(correo);
 evento_blur_vacio(tel);
+evento_blur_email(correo);
 /*
 ficha.addEventListener('blur', () => {
   if (!patt_str.test(ficha.value)) {
